d15d6baf2556d8302287bb9adf9954cf9a3293cb
Alexander Vershilov <alexander.vershilov@gmail.com>
2013-04-08 09:53:58 +0400

    use FixedPrim from Data.ByteString.Builder.Prim

--- Data/StrictPut.hs
+++ Data/StrictPut.hs
@@ -52,6 +52,8 @@ module Data.StrictPut (
 import Control.Applicative
 import qualified Data.ByteString as S
 import qualified Data.ByteString.Internal as S
+import Data.ByteString.Builder.Prim
+import Data.ByteString.Builder.Prim.Internal
 import GHC.Word
 import Foreign
 import GHC.Exts
@@ -87,68 +89,32 @@ instance Monad PutM where
   {-# INLINE (>>=) #-}
   
 putWord8 :: Word8 -> Put
-putWord8 !w = PutM (\(!ptr) -> do { poke ptr w; return ((), ptr `plusPtr` 1) })
+putWord8 !w = PutM (\(!ptr) -> do { runF word8 w ptr ; return ((),ptr `plusPtr` 1) })
 {-# INLINE putWord8 #-}
 
 putWord16be :: Word16 -> Put
-putWord16be !w = PutM f
-  where f !ptr =
-          do poke ptr (fromIntegral (shiftr_w16 w 8) :: Word8)
-             poke (ptr `plusPtr` 1) (fromIntegral (w) :: Word8)
-             return ((), ptr `plusPtr` 2)
+putWord16be !w = PutM (\(!ptr) -> do { runF word16BE w ptr ; return ((),ptr `plusPtr` 2)})
 {-# INLINE putWord16be #-}
 
 -- | Write a Word32 in big endian format
 putWord32be :: Word32 -> Put
-putWord32be !w = PutM f
-  where f !p =
-          do poke p (fromIntegral (shiftr_w32 w 24) :: Word8)
-             poke (p `plusPtr` 1) (fromIntegral (shiftr_w32 w 16) :: Word8)
-             poke (p `plusPtr` 2) (fromIntegral (shiftr_w32 w 8) :: Word8)
-             poke (p `plusPtr` 3) (fromIntegral (w) :: Word8)
-             return ((), p `plusPtr` 4)
+putWord32be !w = PutM (\(!ptr) -> do { runF word32BE w ptr; return ((),ptr `plusPtr` 4)})
 {-# INLINE putWord32be #-}
 
 -- | Write a Word64 in big endian format
 putWord64be :: Word64 -> Put
-#if WORD_SIZE_IN_BITS < 64
---
--- To avoid expensive 64 bit shifts on 32 bit machines, we cast to
--- Word32, and write that
---
-putWord64be !w =
-    let a = fromIntegral (shiftr_w64 w 32) :: Word32
-        b = fromIntegral w :: Word32
-    in PutM $ \(!p) -> do
-      poke p (fromIntegral (shiftr_w32 a 24) :: Word8)
-      poke (p `plusPtr` 1) (fromIntegral (shiftr_w32 a 16) :: Word8)
-      poke (p `plusPtr` 2) (fromIntegral (shiftr_w32 a 8) :: Word8)
-      poke (p `plusPtr` 3) (fromIntegral (a) :: Word8)
-      poke (p `plusPtr` 4) (fromIntegral (shiftr_w32 b 24) :: Word8)
-      poke (p `plusPtr` 5) (fromIntegral (shiftr_w32 b 16) :: Word8)
-      poke (p `plusPtr` 6) (fromIntegral (shiftr_w32 b 8) :: Word8)
-      poke (p `plusPtr` 7) (fromIntegral (b) :: Word8)
-      return ((), p `plusPtr` 8)
-#else
-putWord64be !w = PutM $ \(!p) -> do
-  poke p (fromIntegral (shiftr_w64 w 56) :: Word8)
-  poke (p `plusPtr` 1) (fromIntegral (shiftr_w64 w 48) :: Word8)
-  poke (p `plusPtr` 2) (fromIntegral (shiftr_w64 w 40) :: Word8)
-  poke (p `plusPtr` 3) (fromIntegral (shiftr_w64 w 32) :: Word8)
-  poke (p `plusPtr` 4) (fromIntegral (shiftr_w64 w 24) :: Word8)
-  poke (p `plusPtr` 5) (fromIntegral (shiftr_w64 w 16) :: Word8)
-  poke (p `plusPtr` 6) (fromIntegral (shiftr_w64 w 8) :: Word8)
-  poke (p `plusPtr` 7) (fromIntegral (w) :: Word8)
-  return ((), p `plusPtr` 8)
-#endif
+putWord64be !w = PutM (\(!ptr) -> do { runF word64BE w ptr; return ((),ptr `plusPtr` 8)})
 {-# INLINE putWord64be #-}
 
 putByteString :: S.ByteString -> Put
 putByteString !bs = PutM f
-  where f !ptr =
-          let (fp, offset, len) = S.toForeignPtr bs
-          in do withForeignPtr fp $ \bsptr -> S.memcpy ptr (bsptr `plusPtr` offset) (fromIntegral len)
-                return ((), ptr `plusPtr` len)
+  where f !ptr = case S.toForeignPtr bs of
+                     (fp, offset, len) -> do
+                         withForeignPtr fp $ \bsptr ->
+                           S.memcpy ptr (bsptr `plusPtr` offset)
+                                        (fromIntegral len)
+                         return ((),ptr `plusPtr`  len)
+
 {-# INLINE putByteString #-}
 
 -- | get current address
