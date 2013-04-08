18921f4655926f3a84992c9bfee3d6d2c8155549
Alexander Vershilov <alexander.vershilov@gmail.com>
2013-04-08 18:09:33 +0400

    add shrink operator

--- Data/StrictPut.hs
+++ Data/StrictPut.hs
@@ -34,6 +34,7 @@ module Data.StrictPut (
   marker,
   toAddr,
   distance,
+  shrink,
   -- * Delay
   DelayedPut,
   undelay,
@@ -166,6 +167,11 @@ distance :: Marker
          -> PutM Int
 distance (Marker x) = PutM $ \x' -> return (x' `minusPtr` x, x')
 {-# INLINE distance #-}
+
+shrink :: Marker
+        -> Put
+shrink (Marker x) = PutM $ \_ -> return  ((),x)
+{-# INLINE shrink #-}
 
 -- | Get real address
 toAddr :: Marker -> Addr#
