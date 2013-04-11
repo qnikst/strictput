8c653942e8809b27008787fbdc55d80ba2aeb42e
Alexander Vershilov <alexander.vershilov@gmail.com>
2013-04-12 00:02:08 +0400

    add another approach to delayed input

--- Data/StrictPut.hs
+++ Data/StrictPut.hs
@@ -41,6 +41,13 @@ module Data.StrictPut (
   contramap,
   delayedWord8,
   delayedWord16be,
+  -- ** Another approach
+  DelayedPut',
+  DPut(..),
+  Word16be(..),
+  delayedWord8',
+  delayedWord16be',
+  undelay',
   -- * Buffer
   runPutToBuffer,
   Buffer,
@@ -240,3 +247,35 @@ mkBuffer size = do
 
 runPutToBuffer :: Buffer -> Put -> IO Buffer
 runPutToBuffer (Buffer f p x) put = runPut p put >>= \i -> return (Buffer f (p `plusPtr`i)  (x+i))
+
+newtype DelayedPut' a = DelayedPut' (Ptr a)
+
+class DPut a where
+  dput :: Ptr a -> a -> IO ()
+
+instance DPut Word8 where
+  dput = poke
+  {-# INLINE dput #-}
+
+instance DPut Word16be where
+  dput p (Word16be x) = void (runPut (castPtr p) (putWord16be x))
+  {-# INLINE dput #-}
+
+delayedWord8' :: PutM (DelayedPut' Word8)
+delayedWord8' = PutM $ \p -> poke p (0::Word8) >> 
+                             return (DelayedPut' p, p `plusPtr` 1)
+{-# INLINE delayedWord8' #-}
+
+delayedWord16be' :: PutM (DelayedPut' Word16be)
+delayedWord16be' = PutM $ \p -> poke (castPtr p) (0::Word16) >>
+                                return (DelayedPut' (castPtr p), p `plusPtr` 2)
+{-# INLINE delayedWord16be' #-}
+
+newtype Word16be = Word16be Word16
+
+undelay' :: (DPut a)
+         => DelayedPut' a
+         -> a
+         -> PutM ()
+undelay' (DelayedPut' a) !x = PutM $ \p -> dput a x >> return ((),p)
+{-# INLINE undelay' #-}
