d91dee3ae8c48c70732f43f7c440b979a63525b5
Alexander Vershilov <alexander.vershilov@gmail.com>
2013-04-12 10:13:33 +0400

    fixes in benchmark

--- bench/delayed-input.hs
+++ bench/delayed-input.hs
@@ -1,46 +1,47 @@
+import Control.Monad
 import Data.StrictPut
 import Criterion.Main
 
 test0 w = runPutToByteString 32768 $ do
-    x <- delayedWord8
-    undelay x w 
+    xs <- replicateM (length w) delayedWord8
+    mapM_ (uncurry undelay) $ zip xs w
 
 test1 w =  runPutToByteString 32768 $ do
-    x2 <- delayedWord16be
-    undelay x2 w
+    xs <- replicateM (length w) delayedWord16be
+    mapM_ (uncurry undelay) $ zip xs w
 
 test2 w = runPutToByteString 32768 $ do
-    x <- delayedWord16be
-    x1 <- delayedWord8
-    undelay x1 (fromIntegral w)
-    undelay x w
+    xs1 <- replicateM (length w) delayedWord16be
+    xs2 <- replicateM (length w) delayedWord8
+    mapM_ (uncurry undelay) $ zip xs1 (map fromIntegral w)
+    mapM_ (uncurry undelay) $ zip xs2 w
 
 
 test0' w = runPutToByteString 32768 $ do
-    x <- delayedWord8'
-    undelay' x w 
+    xs <- replicateM (length w) delayedWord8'
+    mapM_ (uncurry undelay') $ zip xs w
 
 test1' w =  runPutToByteString 32768 $ do
-    x2 <- delayedWord16be'
-    undelay' x2 (Word16be w)
+    xs <- replicateM (length w) delayedWord16be'
+    mapM_ (uncurry undelay') $ zip xs (map Word16be w)
 
 test2' w = runPutToByteString 32768 $ do
-    x <- delayedWord16be'
-    x1 <- delayedWord8'
-    undelay' x1 (fromIntegral w)
-    undelay' x (Word16be w)
+    xs1 <- replicateM (length w) delayedWord16be'
+    xs2 <- replicateM (length w) delayedWord8'
+    mapM_ (uncurry undelay') $ zip xs1 (map (Word16be . fromIntegral) w)
+    mapM_ (uncurry undelay') $ zip xs2 w
 
 main = defaultMain 
   [ bgroup "word8"
-      [ bench "closure"  $ nf (map test0) [1..256]
-      , bench "pointer"  $ nf (map test0') [1..256]
+      [ bench "closure"  $ whnf test0 [1..2560]
+      , bench "pointer"  $ whnf test0' [1..2560]
       ]
   , bgroup "word16"
-      [ bench "closure" $ nf (map test1) [1..256]
-      , bench "pointer" $ nf (map test1') [1..256]
+      [ bench "closure" $ whnf test1 [1..2560]
+      , bench "pointer" $ whnf test1' [1..2560]
       ]
   , bgroup "word8+word16"
-      [ bench "delayed word8+word16" $ nf (map test2) [1..256]
-      , bench "delayed word8+word16" $ nf (map test2') [1..256]
+      [ bench "closure" $ whnf test2 [1..2560]
+      , bench "pointer" $ whnf test2' [1..2560]
       ]
   ]
