82f2f952c577850e13b93b874c2d97c01577adc3
Alexander Vershilov <alexander.vershilov@gmail.com>
2013-04-18 13:01:23 +0400

    add test to cabal file

--- test/delayed-put.hs
+++ test/delayed-put.hs
@@ -10,12 +10,13 @@ import Test.Hspec
 import Test.Hspec.QuickCheck
 import Test.QuickCheck.Assertions
 import Test.QuickCheck.Property
-import Test.QuickCheck.Monadic
 import Test.QuickCheck hiding (Result)
 
 import Data.StrictPut
 
+test_dputWord8    :: [Word8] -> Result
 test_dputWord8   ws  = (runPutToByteString 32768 $ mapM_ (\w -> delayedWord8 >>= \x -> undelay x w) ws) ?== BS.pack ws
+test_dputWord16be :: [Word16] -> Result
 test_dputWord16be ws = (runPutToByteString 32768 $ mapM_ (\w -> delayedWord16be >>= \x -> undelay x w) ws) 
     ?== (BS.concat . LBS.toChunks . toLazyByteString $ foldl' (\x y -> x <> word16BE y) mempty ws)
 test_contravariant :: Word8 -> Result
