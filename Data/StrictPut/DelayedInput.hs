a1e688e15603ec4a71ec3bc235310268df8d52d7
Alexander Vershilov <alexander.vershilov@gmail.com>
2013-04-18 11:48:36 +0400

    add DelayedClosure datatype

--- Data/StrictPut/DelayedInput.hs
+++ Data/StrictPut/DelayedInput.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE BangPatterns, MultiParamTypeClasses #-}
+{-# LANGUAGE BangPatterns, MultiParamTypeClasses, FlexibleInstances #-}
 -- | Module provides delayed input functionality,
 -- this basically means that you can input delayed
 -- action (at the moment it will be filled with zeros)
@@ -52,10 +52,12 @@ delayedWord16be = PutM $ \p -> poke (castPtr p) (0::Word16) >>
                                return (DelayedPut $ runPut p . putWord16be, p `plusPtr` 2)
 {-# INLINE delayedWord16be #-}
 
+-- | Wrapper for delayed variable
 newtype DelayedPut'' a b = DelayedPut'' (Ptr Word8)
 
 class Delayed a b c where
   undelay'' :: a b c -> c -> Put
+
 
 instance Delayed DelayedPut'' Word8 Word8 where
   undelay'' (DelayedPut'' a) !x = PutM $ \p -> poke a x >> return ((),p)
@@ -72,3 +74,15 @@ delayedWord8'' = PutM $ \p -> poke p (0::Word8) >> return (DelayedPut'' p,p `plu
 delayedWord16be'' :: PutM (DelayedPut'' Word16be Word16)
 delayedWord16be'' = PutM $ \p -> poke (castPtr p) (0::Word16) >> return (DelayedPut'' p,p `plusPtr` 2)
 {-# INLINE delayedWord16be'' #-}
+
+-- | Wrapper for delayed function, this approach is slitly less efficient
+-- hovewer it supports 'contramap'
+newtype DelayedClosure a b = DelayedClosure (b -> IO Int)
+
+instance Contravariant (DelayedClosure a) where
+  contramap f (DelayedClosure g) = DelayedClosure (g.f)
+  {-# INLINE contramap #-}
+
+instance Delayed DelayedClosure a b where
+  undelay'' (DelayedClosure f) !x = PutM $ \p -> f x >> return ((),p)
+  {-# INLINE undelay'' #-}
