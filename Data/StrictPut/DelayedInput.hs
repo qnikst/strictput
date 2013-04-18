7229685b56b1f727929c4492dfc76d80a2f2bb04
Alexander Vershilov <alexander.vershilov@gmail.com>
2013-04-18 12:04:38 +0400

    remove old versions of DelayedInput API

--- Data/StrictPut/DelayedInput.hs
+++ Data/StrictPut/DelayedInput.hs
@@ -8,16 +8,13 @@
 -- prefixed data in case if you don't know the length 
 -- at begining of put.
 module Data.StrictPut.DelayedInput
-  ( -- * closure
-    DelayedPut
+  ( -- * datatypes and classes
+    DelayedPut(..)
+  , DelayedClosure(..)
+  , Delayed(..)
+  -- * methods
   , delayedWord8
   , delayedWord16be
-  , undelay
-  -- * multiparam typeclass
-  , DelayedPut''(..)
-  , Delayed(..)
-  , delayedWord8''
-  , delayedWord16be''
   -- * reexports
   , Contravariant(..)
   ) where
@@ -30,50 +27,27 @@ import GHC.Word
 import Data.StrictPut.Types
 import Data.StrictPut.PutM
 
--- | Delayed action.
-newtype DelayedPut a = DelayedPut (a -> IO Int)
-
-instance Contravariant DelayedPut where
-  contramap f (DelayedPut g) = DelayedPut (g.f)
-
-undelay :: DelayedPut a 
-        -> a 
-        -> PutM ()
-undelay (DelayedPut f) !x = PutM $ \p -> f x >> return ((),p)
-{-# INLINE undelay #-}
-
-delayedWord8 :: PutM (DelayedPut Word8)
-delayedWord8 = PutM $ \p -> poke p (0::Word8) >> 
-                            return (DelayedPut $ runPut p . putWord8, p `plusPtr` 1)
-{-# INLINE delayedWord8 #-}
-
-delayedWord16be :: PutM (DelayedPut Word16)
-delayedWord16be = PutM $ \p -> poke (castPtr p) (0::Word16) >>
-                               return (DelayedPut $ runPut p . putWord16be, p `plusPtr` 2)
-{-# INLINE delayedWord16be #-}
-
 -- | Wrapper for delayed variable
-newtype DelayedPut'' a b = DelayedPut'' (Ptr Word8)
+newtype DelayedPut a b = DelayedPut (Ptr Word8)
 
 class Delayed a b c where
-  undelay'' :: a b c -> c -> Put
+  undelay :: a b c -> c -> Put
 
+instance Delayed DelayedPut Word8 Word8 where
+  undelay (DelayedPut a) !x = PutM $ \p -> poke a x >> return ((),p)
+  {-# INLINE undelay #-}
 
-instance Delayed DelayedPut'' Word8 Word8 where
-  undelay'' (DelayedPut'' a) !x = PutM $ \p -> poke a x >> return ((),p)
-  {-# INLINE undelay'' #-}
+instance Delayed DelayedPut Word16be Word16 where
+  undelay (DelayedPut a) !x = PutM $ \p -> runPut a (putWord16be x) >> return ((),p)
+  {-# INLINE undelay #-}
 
-instance Delayed DelayedPut'' Word16be Word16 where
-  undelay'' (DelayedPut'' a) !x = PutM $ \p -> runPut a (putWord16be x) >> return ((),p)
-  {-# INLINE undelay'' #-}
+delayedWord8 :: PutM (DelayedPut Word8 Word8)
+delayedWord8 = PutM $ \p -> poke p (0::Word8) >> return (DelayedPut p,p `plusPtr` 1)
+{-# INLINE delayedWord8 #-}
 
-delayedWord8'' :: PutM (DelayedPut'' Word8 Word8)
-delayedWord8'' = PutM $ \p -> poke p (0::Word8) >> return (DelayedPut'' p,p `plusPtr` 1)
-{-# INLINE delayedWord8'' #-}
-
-delayedWord16be'' :: PutM (DelayedPut'' Word16be Word16)
-delayedWord16be'' = PutM $ \p -> poke (castPtr p) (0::Word16) >> return (DelayedPut'' p,p `plusPtr` 2)
-{-# INLINE delayedWord16be'' #-}
+delayedWord16be :: PutM (DelayedPut Word16be Word16)
+delayedWord16be = PutM $ \p -> poke (castPtr p) (0::Word16) >> return (DelayedPut p,p `plusPtr` 2)
+{-# INLINE delayedWord16be #-}
 
 -- | Wrapper for delayed function, this approach is slitly less efficient
 -- hovewer it supports 'contramap'
@@ -84,5 +58,5 @@ instance Contravariant (DelayedClosure a) where
   {-# INLINE contramap #-}
 
 instance Delayed DelayedClosure a b where
-  undelay'' (DelayedClosure f) !x = PutM $ \p -> f x >> return ((),p)
-  {-# INLINE undelay'' #-}
+  undelay (DelayedClosure f) !x = PutM $ \p -> f x >> return ((),p)
+  {-# INLINE undelay #-}
