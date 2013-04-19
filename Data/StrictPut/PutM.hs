c1889e8d1d579c90384b6baf3d82298a797a9942
Alexander Vershilov <alexander.vershilov@gmail.com>
2013-04-19 17:41:56 +0400

    bring back non bytestring version

--- Data/StrictPut/PutM.hs
+++ Data/StrictPut/PutM.hs
@@ -16,40 +16,89 @@ module Data.StrictPut.PutM
 
 import qualified Data.ByteString as S
 import qualified Data.ByteString.Internal as S
-import Data.ByteString.Builder.Prim
-import Data.ByteString.Builder.Prim.Internal
 import GHC.Word
 import Foreign
 import GHC.Exts
 import Data.StrictPut.Types
 
 putWord8 :: Word8 -> Put
-putWord8 = encodeWithF word8
+putWord8 !w = PutM (\(!ptr) -> do { poke ptr w; return ((), ptr `plusPtr` 1) })
 {-# INLINE putWord8 #-}
 
 putWord16be :: Word16 -> Put
-putWord16be = encodeWithF word16BE
+putWord16be !w = PutM f
+  where f !ptr =
+          do poke ptr (fromIntegral (shiftr_w16 w 8) :: Word8)
+             poke (ptr `plusPtr` 1) (fromIntegral (w) :: Word8)
+             return ((), ptr `plusPtr` 2)
 {-# INLINE putWord16be #-}
 
 -- | Write a Word32 in big endian format
 putWord32be :: Word32 -> Put
-putWord32be = encodeWithF word32BE
+putWord32be !w = PutM f
+  where f !p =
+          do poke p (fromIntegral (shiftr_w32 w 24) :: Word8)
+             poke (p `plusPtr` 1) (fromIntegral (shiftr_w32 w 16) :: Word8)
+             poke (p `plusPtr` 2) (fromIntegral (shiftr_w32 w 8) :: Word8)
+             poke (p `plusPtr` 3) (fromIntegral (w) :: Word8)
+             return ((), p `plusPtr` 4)
 {-# INLINE putWord32be #-}
 
 -- | Write a Word64 in big endian format
 putWord64be :: Word64 -> Put
-putWord64be = encodeWithF word64BE
+#if WORD_SIZE_IN_BITS < 64
+--
+-- To avoid expensive 64 bit shifts on 32 bit machines, we cast to
+-- Word32, and write that
+--
+putWord64be !w =
+    let a = fromIntegral (shiftr_w64 w 32) :: Word32
+        b = fromIntegral w :: Word32
+    in PutM $ \(!p) -> do
+      poke p (fromIntegral (shiftr_w32 a 24) :: Word8)
+      poke (p `plusPtr` 1) (fromIntegral (shiftr_w32 a 16) :: Word8)
+      poke (p `plusPtr` 2) (fromIntegral (shiftr_w32 a 8) :: Word8)
+      poke (p `plusPtr` 3) (fromIntegral (a) :: Word8)
+      poke (p `plusPtr` 4) (fromIntegral (shiftr_w32 b 24) :: Word8)
+      poke (p `plusPtr` 5) (fromIntegral (shiftr_w32 b 16) :: Word8)
+      poke (p `plusPtr` 6) (fromIntegral (shiftr_w32 b 8) :: Word8)
+      poke (p `plusPtr` 7) (fromIntegral (b) :: Word8)
+      return ((), p `plusPtr` 8)
+#else
+putWord64be !w = PutM $ \(!p) -> do
+  poke p (fromIntegral (shiftr_w64 w 56) :: Word8)
+  poke (p `plusPtr` 1) (fromIntegral (shiftr_w64 w 48) :: Word8)
+  poke (p `plusPtr` 2) (fromIntegral (shiftr_w64 w 40) :: Word8)
+  poke (p `plusPtr` 3) (fromIntegral (shiftr_w64 w 32) :: Word8)
+  poke (p `plusPtr` 4) (fromIntegral (shiftr_w64 w 24) :: Word8)
+  poke (p `plusPtr` 5) (fromIntegral (shiftr_w64 w 16) :: Word8)
+  poke (p `plusPtr` 6) (fromIntegral (shiftr_w64 w 8) :: Word8)
+  poke (p `plusPtr` 7) (fromIntegral (w) :: Word8)
+  return ((), p `plusPtr` 8)
+#endif
 {-# INLINE putWord64be #-}
 
 putByteString :: S.ByteString -> Put
 putByteString !bs = PutM f
-  where f !ptr = case S.toForeignPtr bs of
-                     (fp, offset, len) -> do
-                         withForeignPtr fp $ \bsptr ->
-                           S.memcpy ptr (bsptr `plusPtr` offset)
-                                        (fromIntegral len)
-                         return ((),ptr `plusPtr`  len)
+  where f !ptr =
+          let (fp, offset, len) = S.toForeignPtr bs
+          in do withForeignPtr fp $ \bsptr -> S.memcpy ptr (bsptr `plusPtr` offset) (fromIntegral len)
+                return ((), ptr `plusPtr` len)
 {-# INLINE putByteString #-}
 
-encodeWithF :: FixedPrim a -> a -> Put
-encodeWithF fixedPrim w = PutM $ \(!ptr) -> runF fixedPrim w ptr >> return ((), ptr `plusPtr` (size fixedPrim))
+{-# INLINE shiftr_w16 #-}
+shiftr_w16 :: Word16 -> Int -> Word16
+{-# INLINE shiftr_w32 #-}
+shiftr_w32 :: Word32 -> Int -> Word32
+{-# INLINE shiftr_w64 #-}
+shiftr_w64 :: Word64 -> Int -> Word64
+
+
+#if defined(__GLASGOW_HASKELL__) && !defined(__HADDOCK__)
+shiftr_w16 (W16# w) (I# i) = W16# (w `uncheckedShiftRL#` i)
+shiftr_w32 (W32# w) (I# i) = W32# (w `uncheckedShiftRL#` i)
+
+#if WORD_SIZE_IN_BITS < 64
+shiftr_w64 (W64# w) (I# i) = W64# (w `uncheckedShiftRL64#` i)
+#endif
+#endif
